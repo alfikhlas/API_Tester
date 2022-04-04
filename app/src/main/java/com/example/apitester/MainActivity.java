6ce93bcb5346476c42d90ab8eca25101942475cf
Alfikhlas <alfikhlas@gmail.com>
2022-04-04 08:27:55 +0700

    Recycler View

--- app/src/main/java/com/example/apitester/MainActivity.java
+++ app/src/main/java/com/example/apitester/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.apitester;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -11,18 +13,31 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    String apiNames[], links[];
+
+    RecyclerView recyclerView;
+
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv1 = findViewById(R.id.Testing);
+        recyclerView = findViewById(R.id.recylerView);
+
+        apiNames = getResources().getStringArray(R.array.apiNames);
+        links = getResources().getStringArray(R.array.links);
+
+        viewAdapter viewAdapter = new viewAdapter(this, apiNames, links);
+
+        recyclerView.setAdapter(viewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                tv1.setText("Hello");
+                View x = recyclerView.getLayoutManager().findViewByPosition(0);
             }
         });
     }
--- app/src/main/java/com/example/apitester/viewAdapter.java
+++ app/src/main/java/com/example/apitester/viewAdapter.java
@@ -1,2 +1,77 @@
-package com.example.apitester;public class viewAdapter {
+package com.example.apitester;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class viewAdapter extends RecyclerView.Adapter<viewAdapter.viewHolder> {
+
+    String data1[], data2[];
+    Context context;
+    private RequestQueue queue;
+
+    public viewAdapter(Context ct, String apiNames[], String links[]){
+        context = ct;
+        data1 = apiNames;
+        data2 = links;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.view_row, parent, false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewAdapter.viewHolder holder, int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.text3 = holder.myText3;
+        text3.setText("NONE");
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        TextView myText1, myText2;
+        TextView myText3;
+        ConstraintLayout mainLayout;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.apiNamesTxt);
+            myText2 = itemView.findViewById(R.id.linksTxt);
+            myText3 = itemView.findViewById(R.id.statusTxt);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+
+    public void setStatus(@NonNull viewAdapter.viewHolder holder, String status){
+        holder.myText3.setText(status);
+    }
 }
